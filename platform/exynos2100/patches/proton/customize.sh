5ebe027fde53472f3e01a995385d94348a8488e4
Daubfy <149339273+Daubfy@users.noreply.github.com>
2026-01-19 21:41:48 -0500

    Add Proton+ Kernel
    Co-Authored-By: EliteGalaxy <142471606+elitetest1@users.noreply.github.com>

---
+++ platform/exynos2100/patches/proton/customize.sh
@@ -0,0 +1,3 @@
+cp -fa "$SRC_DIR/platform/exynos2100/patches/proton/img/boot.img" "$WORK_DIR/kernel/boot.img"
+cp -fa "$SRC_DIR/platform/exynos2100/patches/proton/img/vendor_boot.img" "$WORK_DIR/kernel/vendor_boot.img"
+
